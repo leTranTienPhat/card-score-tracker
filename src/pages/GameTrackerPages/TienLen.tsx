81dfba73d665e48d432f26eb382c1201c483baf4
Lê Trần Tiến Phát <tienphat0707@gmail.com>
2023-06-07 10:54:48 +0700

    Update Round Ranking Function

--- src/pages/GameTrackerPages/TienLen.tsx
+++ src/pages/GameTrackerPages/TienLen.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, memo } from 'react'
+import { useState, useEffect } from 'react'
 import { IPlayer } from '../../models/playerModel';
 import { useNavigate } from 'react-router-dom';
 import RoundRanking from '../../components/RoundRanking';
